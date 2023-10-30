35c5d39f400c0e312732e60f7c4e9ade0345827f
Gerald Ssebaggala <ssgerald710@gmail.com>
2023-10-30 14:54:06 +0300

    improve functionality & displayability

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const randomIdea = document.querySelector(".bot--url");
 const image = document.querySelector(".card--img");
 const button = document.querySelector("button");
+const education = document.querySelector(".visit-course");
+const educationLink = document.querySelector(".course--link");
+const educationFee = document.querySelector(".course--price");
+
+console.log(educationFee, educationLink)
+
+// fetch("https://apis.scrimba.com/bored/api/activity")
 
 button.addEventListener("click", () => {
-    fetch("https://apis.scrimba.com/bored/api/activity")
+    fetch("http://www.boredapi.com/api/activity/")
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            console.log(data.link)
             randomIdea.textContent = data.activity;
+
+            if(data.link === ""){
+                education.style.display = "none";
+            } else {
+                education.style.display = "block";
+                education.style.width = "50%";
+                educationLink.innerHTML = `
+                                                Link : <a target="-blank" href="${data.link}" class="course-link">
+                                                    ${data.link}
+                                                </a>`;
+                educationFee.innerHTML = `Price : $ ${data.price}`;
+            }
         })
 })
 
