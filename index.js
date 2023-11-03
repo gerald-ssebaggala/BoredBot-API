722a3a0a932781b6ef3880daabd390be4c9492a1
Gerald Ssebaggala <ssgerald710@gmail.com>
2023-11-03 18:41:09 +0300

    improve response depending on assigned task.

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ button.addEventListener("click", () => {
             console.log(data.link)
             randomIdea.textContent = data.activity;
 
-            if(data.link === ""){
-                courseDisplayer.style.display = "none";
-            } else {
-                courseDisplayer.style.display = "block";
-                courseDisplayer.style.padding = ".5em";
+            if(data.link != ""){
                 courseDisplayer.innerHTML = `
                                                 <p>
                                                     Link : 
@@ -35,5 +31,25 @@ button.addEventListener("click", () => {
                                                 </p>
                                             `;
             }
+
+            if(data.type=== "busywork"){
+                courseDisplayer.innerHTML = `<p>Let me know how it went! 😋</p>`
+            }else if(data.type=== "social"){
+                courseDisplayer.innerHTML = `<p>Some how good for a day! 😉</p>`
+            }else if(data.type=== "education" && data.link === ""){
+                courseDisplayer.innerHTML = `<p>Sorry we couldn't provide you with a link! 😔...Perhaps you may google one. 🙈</p>`
+            }else if(data.type=== "recreational"){
+                courseDisplayer.innerHTML = `<p>How about you give it  a shot see how it goes? 🤪</p>`
+            }else if(data.type=== "cooking"){
+                courseDisplayer.innerHTML = `<p>How about looking up a Recipe on how to go about it? 🍳📜</p>`
+            }else if(data.type=== "music"){
+                courseDisplayer.innerHTML = `<p>Remember music reduces stress 🎶</p>`
+            }else if(data.type=== "charity"){
+                courseDisplayer.innerHTML = `<p>I find charity quite amazing.. Do you? 🫶</p>`
+            }else if(data.type=== "relaxation"){
+                courseDisplayer.innerHTML = `<p>How is that for a Relaxation? 😉</p>`
+            }else {
+                courseDisplayer.innerHTML = `<p>I hope all goes well! 🫶😋</p>`
+            }
         })
-})
+});
